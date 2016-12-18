78f45ed25e0cc3ac054aaf9c15da3d0da1d2e967
Juan Carlos Castaño Moreno <11400.es@gmail.com>
2016-12-18 19:31:14 +0100

    Añadimos las imagenes al admin

--- imagenes/admin.py
+++ imagenes/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from imagenes.models import Imagenes
 
 # Register your models here.
+
+
+class ImagenesAdmin(admin.ModelAdmin):
+
+    list_display = ('nombre', 'url')
+admin.site.register(Imagenes, ImagenesAdmin)
